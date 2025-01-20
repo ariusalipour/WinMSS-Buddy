353218daa7731646c841e9004ba4f8989448fba7
Aryan Alipour <aryan.alipour@outlook.com>
2025-01-20 00:45:05 +0000

    Fix Scores Scroll

--- winmss-buddy-web/src/views/MatchesView.tsx
+++ winmss-buddy-web/src/views/MatchesView.tsx
@@ -34,10 +34,7 @@ const MatchesView: React.FC = () => {
                         <TabPane tab={match.matchName} key={`match-${matchIndex}`}>
                             <Tabs defaultActiveKey="1">
                                 {/* Competitors Tab */}
-                                <TabPane
-                                    tab={`Competitors (${competitorCount})`}
-                                    key="1"
-                                >
+                                <TabPane tab={`Competitors (${competitorCount})`} key="1">
                                     <Table
                                         dataSource={processedData.registrations
                                             .filter((reg) => reg.matchId === match.matchId)
@@ -118,8 +115,8 @@ const MatchesView: React.FC = () => {
                                             .filter((stage) => stage.matchId === match.matchId)
                                             .map((stage) => {
                                                 const scoreCount = processedData.scores.filter(
-                                                    (score) => score.stageId === stage.stageId
-                                                ).length;
+                                                    (score) => score.stageId === stage.stageId && score.matchId === match.matchId
+                                                ).length; // Filter scores by both stageId and matchId
 
                                                 return {
                                                     stageNumber: stage.stageId,
@@ -154,9 +151,18 @@ const MatchesView: React.FC = () => {
                                 {/* Squads Tab */}
                                 <TabPane tab={`Squads (${squadCount})`} key="3">
                                     <Table
-                                        dataSource={processedData.squads.filter(
-                                            (squad) => squad.matchId === match.matchId
-                                        )}
+                                        dataSource={processedData.squads
+                                            .filter((squad) => squad.matchId === match.matchId)
+                                            .map((squad) => {
+                                                const registrationCount = processedData.registrations.filter(
+                                                    (reg) => reg.squadId === squad.squadId
+                                                ).length;
+
+                                                return {
+                                                    squadName: squad.squadName,
+                                                    noInSquad: registrationCount,
+                                                };
+                                            })}
                                         columns={[
                                             {
                                                 title: "Squad Name",
@@ -165,10 +171,10 @@ const MatchesView: React.FC = () => {
                                                 sorter: (a, b) => a.squadName.localeCompare(b.squadName),
                                             },
                                             {
-                                                title: "Squad Number",
-                                                dataIndex: "squadNumber",
-                                                key: "squadNumber",
-                                                sorter: (a, b) => a.squadNumber - b.squadNumber,
+                                                title: "No in Squad",
+                                                dataIndex: "noInSquad",
+                                                key: "noInSquad",
+                                                sorter: (a, b) => a.noInSquad - b.noInSquad,
                                             },
                                         ]}
                                         bordered
@@ -178,27 +184,135 @@ const MatchesView: React.FC = () => {
                                 {/* Scores Tab */}
                                 <TabPane tab={`Scores (${scoreCount})`} key="4">
                                     <Table
-                                        dataSource={processedData.scores.filter(
-                                            (score) => score.matchId === match.matchId
-                                        )}
+                                        scroll={{ x: "100%" }}
+                                        dataSource={processedData.scores
+                                            .filter((score) => score.matchId === match.matchId)
+                                            .map((score) => {
+                                                const stage = processedData.stages.find(
+                                                    (stage) => stage.stageId === score.stageId
+                                                );
+                                                const registration = processedData.registrations.find(
+                                                    (reg) =>
+                                                        reg.memberId === score.memberId &&
+                                                        reg.matchId === score.matchId
+                                                );
+                                                const competitor = processedData.competitors.find(
+                                                    (comp) => comp.memberId === score.memberId
+                                                );
+
+                                                const percentage =
+                                                    score.stagePoints && score.totalStagePoints
+                                                        ? ((score.stagePoints / score.totalStagePoints) * 100).toFixed(2)
+                                                        : "N/A";
+
+                                                return {
+                                                    stageNumber: stage?.stageId || "N/A",
+                                                    firstName: competitor?.firstname || "N/A",
+                                                    lastName: competitor?.lastname || "N/A",
+                                                    division: registration?.divisionId || "N/A",
+                                                    category: registration?.categoryId || "N/A",
+                                                    percentage,
+                                                    time: score.shootTime || "N/A",
+                                                    stagePoints: score.finalScore || "N/A",
+                                                    hitFactor: score.hitFactor.toFixed(2) || "N/A",
+                                                    alpha: score.scoreA || 0,
+                                                    beta: score.scoreB || 0,
+                                                    charlie: score.scoreC || 0,
+                                                    delta: score.scoreD || 0,
+                                                    mike: score.misses || 0,
+                                                    penalty: score.penalties || 0,
+                                                };
+                                            })}
                                         columns={[
                                             {
-                                                title: "Competitor Name",
-                                                dataIndex: "competitorName",
-                                                key: "competitorName",
-                                                sorter: (a, b) => a.competitorName.localeCompare(b.competitorName),
+                                                title: "Stage No",
+                                                dataIndex: "stageNumber",
+                                                key: "stageNumber",
+                                                sorter: (a, b) => a.stageNumber - b.stageNumber,
                                             },
                                             {
-                                                title: "Score",
-                                                dataIndex: "score",
-                                                key: "score",
-                                                sorter: (a, b) => a.score - b.score,
+                                                title: "First Name",
+                                                dataIndex: "firstName",
+                                                key: "firstName",
+                                                sorter: (a, b) => a.firstName.localeCompare(b.firstName),
+                                            },
+                                            {
+                                                title: "Last Name",
+                                                dataIndex: "lastName",
+                                                key: "lastName",
+                                                sorter: (a, b) => a.lastName.localeCompare(b.lastName),
+                                            },
+                                            {
+                                                title: "Division",
+                                                dataIndex: "division",
+                                                key: "division",
+                                                sorter: (a, b) => a.division - b.division,
+                                            },
+                                            {
+                                                title: "Category",
+                                                dataIndex: "category",
+                                                key: "category",
+                                                sorter: (a, b) => a.category - b.category,
+                                            },
+                                            {
+                                                title: "Percentage",
+                                                dataIndex: "percentage",
+                                                key: "percentage",
+                                                sorter: (a, b) => parseFloat(a.percentage) - parseFloat(b.percentage),
                                             },
                                             {
                                                 title: "Time",
                                                 dataIndex: "time",
                                                 key: "time",
                                                 sorter: (a, b) => a.time - b.time,
+                                            },
+                                            {
+                                                title: "Stage Points",
+                                                dataIndex: "stagePoints",
+                                                key: "stagePoints",
+                                                sorter: (a, b) => a.stagePoints - b.stagePoints,
+                                            },
+                                            {
+                                                title: "Hit Factor",
+                                                dataIndex: "hitFactor",
+                                                key: "hitFactor",
+                                                sorter: (a, b) => a.hitFactor - b.hitFactor,
+                                            },
+                                            {
+                                                title: "Alpha",
+                                                dataIndex: "alpha",
+                                                key: "alpha",
+                                                sorter: (a, b) => a.alpha - b.alpha,
+                                            },
+                                            {
+                                                title: "Beta",
+                                                dataIndex: "beta",
+                                                key: "beta",
+                                                sorter: (a, b) => a.beta - b.beta,
+                                            },
+                                            {
+                                                title: "Charlie",
+                                                dataIndex: "charlie",
+                                                key: "charlie",
+                                                sorter: (a, b) => a.charlie - b.charlie,
+                                            },
+                                            {
+                                                title: "Delta",
+                                                dataIndex: "delta",
+                                                key: "delta",
+                                                sorter: (a, b) => a.delta - b.delta,
+                                            },
+                                            {
+                                                title: "Mike",
+                                                dataIndex: "mike",
+                                                key: "mike",
+                                                sorter: (a, b) => a.mike - b.mike,
+                                            },
+                                            {
+                                                title: "Penalty",
+                                                dataIndex: "penalty",
+                                                key: "penalty",
+                                                sorter: (a, b) => a.penalty - b.penalty,
                                             },
                                         ]}
                                         bordered
